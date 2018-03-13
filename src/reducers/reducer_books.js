95d91a877170d55b297a88353322613cac00a1db
coreydunkin <corey.dunkin@gmail.com>
2018-03-14 00:22:21 +1100

    added projects

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'Javascript', pages: 101},
+		{title: 'Harry Potter', pages: 98},
+		{title: 'The Dark Tower', pages: 5222},
+		{title: 'Eloquent Ruby', pages: 1}
+	]
+}
